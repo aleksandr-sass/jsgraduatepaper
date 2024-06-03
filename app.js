c41663d8904ab0240e6aff7605283d665a7c3a7f
aleksandr-sass <a.sass@tut.by>
2024-06-03 17:54:47 +0300

    feat: minimal viable product was born

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const divResult = document.querySelector('.result');
+const telArr = [];
+
 fetch('https://jsbymda-default-rtdb.firebaseio.com/orders.json')
 .then(response => {
     return response.json();
@@ -6,9 +9,26 @@ fetch('https://jsbymda-default-rtdb.firebaseio.com/orders.json')
      for (const [key, value] of Object.entries(response)) {
         if (key > Date.now() - 16 * 3600000) {
             console.log(`${value.tel}`);
+            telArr.push(value.tel);
         }        
      }
+
+     console.log(telArr);
+     showMeTheGun();
 });
+
+function showMeTheGun() {
+    let text = '';
+    telArr.forEach(el => text += showMeYourID(el));
+    divResult.innerHTML = text;
+}
+
+function showMeYourID(tel) {
+    const realTel = '+' + tel;
+    return `<p><a href="tel:${realTel}">${realTel}</a></p>`;
+}
+
+
 
 // fetch('https://jsbymda-default-rtdb.firebaseio.com/orders.json')
 // .then(response => {

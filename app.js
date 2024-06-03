42fdacb6ecbba9b39aca42fa7e3851708a5f3755
Бойко Виктор Б. <C:\Users\boyko\AppData\Roaming\The Bat!>
2024-06-03 11:44:04 +0300

    feat: 14th task: i am not so far...

--- app.js
+++ app.js
@@ -2,7 +2,28 @@ fetch('https://jsbymda-default-rtdb.firebaseio.com/orders.json')
 .then(response => {
     return response.json();
 })
-.then(response => {
-    console.log(response);
-    console.log(response.length);
+.then(response => {    
+     for (const [key, value] of Object.entries(response)) {
+        if (key > Date.now() - 16 * 3600000) {
+            console.log(`${value.tel}`);
+        }        
+     }
 });
+
+// fetch('https://jsbymda-default-rtdb.firebaseio.com/orders.json')
+// .then(response => {
+//     return response.json();
+// })
+// .then(response => {
+//     console.log(response);
+//     console.log(response.length);
+// });
+
+
+// fetch('https://jsbymda-default-rtdb.firebaseio.com/orders.json')
+// .then(response => {
+//     return response.json();
+// })
+// .then(response => {    
+//      console.log(Object.keys(response).filter(el => el > Date.now() - 16 * 3600000));
+// });
